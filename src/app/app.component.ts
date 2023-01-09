7dc79f594f34ea5e2082c8b39dd967503d682934
Wallace Servato Bertolaccini <wallacebertolaccini@gmail.com>
2023-01-08 21:41:20 -0300

    FirstDay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss','./app.component.css']
 })
 export class AppComponent {
   title = 'DrRaster';
+  isMenuOpen = false;
+   constructor(
+    private matIconRegistry:MatIconRegistry,
+    private domSanitizer:DomSanitizer
+   ){
+    this.matIconRegistry.addSvgIcon(
+      'logo',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/marca real rastreamento.svg')
+    )
+   }
+
+
+   openMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+   }
 }
