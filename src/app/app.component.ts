76a5552792783f8a35aac1c3407da83ac666d43c
Wallace Servato Bertolaccini <wallacebertolaccini@gmail.com>
2023-01-21 14:00:13 -0300

    commit allex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material/icon';
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges,ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss','./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'DrRaster';
   isMenuOpen = false;
+  width = screen.width;
    constructor(
     private matIconRegistry:MatIconRegistry,
-    private domSanitizer:DomSanitizer
+    private domSanitizer:DomSanitizer,
+    private cdr: ChangeDetectorRef
    ){
+      console.log(this.width);
     this.matIconRegistry.addSvgIcon(
       'logo',
       this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/marca real rastreamento.svg')
     )
    }
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
 
 
    openMenu(){
     this.isMenuOpen = !this.isMenuOpen;
    }
+
+
 }
